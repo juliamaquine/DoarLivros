3a7af377b7b880519cd04b593737c90927c486bf
julia maquine <juliamaquine10@gmail.com>
2025-04-09 14:53:31 -0400

    update

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,43 +1,58 @@
+import { useFetcher } from "react-router-dom";
+import Livro1 from "../../assets/Livro1.png";
+import Livro2 from "../../assets/Livro2.png";
+import Livro3 from "../../assets/Livro3.png";
 
-import Livro1 from '../../assets/Livro1.png'
-import Livro2 from '../../assets/Livro2.png'
-import Livro3 from '../../assets/Livro3.png'
+import s from "./LivrosDoados.module.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+export default function LivrosDoados() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        import.meta.env.VITE_BACKEND_URL + "/livros"
+      );
+      setData(response.data);
 
-import s from './LivrosDoados.module.scss'
-
-export default function LivrosDoados(){
-    return(
-   
-     <section className={s.LivrosDoadosSection}> 
-     <h2> Livros Doados</h2>
-     <section className={s.CardsSection}>
+      console.log(response.data);
+    })();
+  }, []);
+  return (
+    <section className={s.LivrosDoadosSection}>
+      <h2> Livros Doados</h2>
+      <section className={s.CardsSection}>
         <section>
-        <img src={Livro1} alt="capa do livro portagonista" />
-        <h1>Diário de Anne Frank</h1>
-        <p>Anne Frank</p>
-        <p>Autobiografia</p>
-</section>
-
+          <img src={Livro1} alt="capa do livro portagonista" />
+          <h1>Diário de Anne Frank</h1>
+          <p>Anne Frank</p>
+          <p>Autobiografia</p>
+        </section>
+        {data.map((v) => {
+          return (
+            <section key={v.id}>
+              <img src={v.imagem_url} alt="capa do livro O Meu Pé de Laranja Lima" />
+              <h1>{v.titulo}</h1>
+              <p>{v.autor}</p>
+              <p>{v.categoria}</p>
+            </section>
+          );
+        })}
         <section>
-        <img src={Livro2} alt="capa do livro O Meu Pé de Laranja Lima" />
-        <h1>O Meu Pé de Laranja Lima</h1>
-        <p>José Mauro de Vasconcelos</p>
-        <p>Ficção</p>
+          <img src={Livro2} alt="capa do livro O Meu Pé de Laranja Lima" />
+          <h1>O Meu Pé de Laranja Lima</h1>
+          <p>José Mauro de Vasconcelos</p>
+          <p>Ficção</p>
         </section>
 
         <section>
-        <img src={Livro3} alt="capa do livro portagonista" />
-        <h1>O protagonista</h1>
-        <p>Susanne Andrade</p>
-        <p>Ficção</p>
+          <img src={Livro3} alt="capa do livro portagonista" />
+          <h1>O protagonista</h1>
+          <p>Susanne Andrade</p>
+          <p>Ficção</p>
+        </section>
+      </section>
     </section>
-     </section>
-     </section>
-   
-     
-     
-     
-    )
-
-   }
+  );
+}
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,24 +1,73 @@
-import iconeLivro from '../../assets/iconeLivro.png'
-import s from './queroDoar.module.scss'
+import { useState } from "react";
+import iconeLivro from "../../assets/iconeLivro.png";
+import s from "./queroDoar.module.scss";
+import axios from "axios";
 
 export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [img, setImg] = useState("");
+
+  async function doar(event) {
+    event.preventDefault()
+    
+    await axios.post(import.meta.env.VITE_BACKEND_URL + "/doar", {
+      titulo: titulo,
+      categoria: categoria,
+      autor: autor,
+      imagem_url: img,
+    });
+  }
   return (
-
-    <section className= {s.queroDoarSection}>
-      <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-      <form action="">
-
+    <section className={s.queroDoarSection}>
+      <p>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro
+      </p>
+      <form>
         <div>
-        <img src={iconeLivro} alt="imagem ilustrando um livro aberto" />
-        <h2> Informações do Livro</h2>
+          <img src={iconeLivro} alt="imagem ilustrando um livro aberto" />
+          <h2> Informações do Livro </h2>
         </div>
-        <input type="text" name="" id="" placeholder='Titulo' />
-        <input type="text" name="" id="" placeholder='Categoria'/>
-        <input type="text" name="" id="" placeholder='Autor'/>
-        <input type="text" name="" id="" placeholder='Link da Imagem'/>
-        <input type="submit" value="Doar"className={s.buttonDoar} />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Titulo"
+          onChange={(event) => {
+            setTitulo(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Categoria"
+          onChange={(event) => {
+            setCategoria(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Autor"
+          onChange={(event) => {
+            setAutor(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Link da Imagem"
+          onChange={(event) => {
+            setImg(event.target.value);
+          }}
+        />
+        <input type="submit" value="Doar" className={s.buttonDoar} onClick={doar} />
       </form>
     </section>
-  )
-
+  );
 }
